89c3d3e00e154870163570b005dfa7928fd9adb0
teguh-scaleocean <teguh.scaleocean@gmail.com>
2024-08-22 15:01:30 +0700

    receipt-detail-inprogress-5

--- lib/presentation/receipt/screens/receipt_detail_screen.dart
+++ lib/presentation/receipt/screens/receipt_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dash/flutter_dash.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:inventory_v3/common/components/custom_app_bar.dart';
 import 'package:inventory_v3/common/components/custom_divider.dart';
@@ -23,147 +24,167 @@ class _ReceiptDetailScreenState extends State<ReceiptDetailScreen> {
     return SafeArea(
       child: Scaffold(
         appBar: const CustomAppBar(title: "Receipt Detail"),
-        body: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Flexible(child: buildOutlineButton(context, label: "Damage")),
-                  Flexible(child: buildOutlineButton(context, label: "Damage")),
-                ],
-              )
-            ],
-          ),
-        ),
-        // body: ListView(
-        //   children: [
-        //     Container(
-        //       padding: const EdgeInsets.all(16.0),
-        //       child: Column(
-        //         children: [
-        //           Row(
-        //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //             children: [
-        //               Text(
-        //                 "WH/IN/00012",
-        //                 style: BaseText.blackText17
-        //                     .copyWith(fontWeight: BaseText.medium),
-        //               ),
-        //               const StatusBadge(
-        //                 status: "Waiting",
-        //                 color: ColorName.waitingColor,
-        //               ),
-        //             ],
-        //           ),
-        //           8.height,
-        //           Row(
-        //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //             children: [
-        //               Text(
-        //                 "Package: Pallet",
-        //                 style: BaseText.grey1Text13
-        //                     .copyWith(fontWeight: BaseText.light),
-        //               ),
-        //               Text(
-        //                 "Tracking: No Tracking",
-        //                 style: BaseText.grey1Text13
-        //                     .copyWith(fontWeight: BaseText.light),
-        //               )
-        //             ],
-        //           ),
-        //         ],
-        //       ),
-        //     ),
-        //     16.height,
-        //     Padding(
-        //       padding: const EdgeInsets.symmetric(horizontal: 16),
-        //       child: Row(
-        //         children: [
-        //           Flexible(
-        //             child: DisableButton(
-        //               height: 40,
-        //               width: double.infinity,
-        //               iconWidget: SvgPicture.asset(
-        //                 LocalImages.scanIcons,
-        //                 color: ColorName.whiteColor,
-        //               ),
-        //               title: "Scan",
-        //             ),
-        //           ),
-        //           16.width,
-        //           Flexible(
-        //             child: DisableButton(
-        //               height: 40,
-        //               width: double.infinity,
-        //               // width: 156,
-        //               iconWidget:
-        //                   SvgPicture.asset(LocalImages.updatePalleteIcons),
-        //               title: "Update Pallete",
-        //             ),
-        //           ),
-        //         ],
-        //       ),
-        //     ),
-        //     16.height,
-        //     const CustomDivider(height: 1.0, color: ColorName.grey9Color),
-        //     16.height,
-        //     Container(
-        //       padding: const EdgeInsets.symmetric(horizontal: 16),
-        //       child: Column(
-        //         crossAxisAlignment: CrossAxisAlignment.start,
-        //         children: [
-        //           RichText(
-        //             text: TextSpan(children: [
-        //               TextSpan(
-        //                 text: "Sch. Date: ",
-        //                 style: BaseText.baseTextStyle.copyWith(
-        //                   fontSize: 14,
-        //                   color: ColorName.dateTimeColor,
-        //                 ),
-        //               ),
-        //               TextSpan(
-        //                 text: "14/06/2024 - 15.30",
-        //                 style: BaseText.baseTextStyle.copyWith(
-        //                   fontSize: 14,
-        //                   fontWeight: BaseText.light,
-        //                   color: ColorName.dateTimeColor,
-        //                 ),
-        //               )
-        //             ]),
-        //           ),
-        //           8.height,
-        //           Row(
-        //             children: [
-        //               Column(
-        //                 crossAxisAlignment: CrossAxisAlignment.center,
-        //                 children: [
-        //                   SvgPicture.asset(LocalImages.receiveIcon),
-        //                   SvgPicture.asset(LocalImages.lineIcon),
-        //                   SvgPicture.asset(LocalImages.destinationIcon),
-        //                 ],
-        //               ),
-        //               16.width,
-        //               Column(
-        //                 children: [
-        //                   buildPlaceStepper(
-        //                     label: "Receive From",
-        //                     location: "Main Storage Area",
-        //                   ),
-        //                   24.height,
-        //                   buildPlaceStepper(
-        //                     label: "Destination Location",
-        //                     location: "Medical Storage",
-        //                   )
-        //                 ],
-        //               ),
-        //             ],
-        //           ),
-        //         ],
-        //       ),
-        //     ),
-        //   ],
+        // body: Container(
+        //   padding: const EdgeInsets.symmetric(horizontal: 16),
+        //   child: const Column(
+        //     children: [],
+        //   ),
         // ),
+        body: ListView(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "WH/IN/00012",
+                        style: BaseText.blackText17
+                            .copyWith(fontWeight: BaseText.medium),
+                      ),
+                      const StatusBadge(
+                        status: "Waiting",
+                        color: ColorName.waitingColor,
+                      ),
+                    ],
+                  ),
+                  8.height,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Package: Pallet",
+                        style: BaseText.grey1Text13
+                            .copyWith(fontWeight: BaseText.light),
+                      ),
+                      Text(
+                        "Tracking: No Tracking",
+                        style: BaseText.grey1Text13
+                            .copyWith(fontWeight: BaseText.light),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            16.height,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                children: [
+                  Flexible(
+                    child: DisableButton(
+                      height: 40,
+                      width: double.infinity,
+                      iconWidget: SvgPicture.asset(
+                        LocalImages.scanIcons,
+                        color: ColorName.whiteColor,
+                      ),
+                      title: "Scan",
+                    ),
+                  ),
+                  16.width,
+                  Flexible(
+                    child: DisableButton(
+                      height: 40,
+                      width: double.infinity,
+                      // width: 156,
+                      iconWidget:
+                          SvgPicture.asset(LocalImages.updatePalleteIcons),
+                      title: "Update Pallete",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            16.height,
+            const CustomDivider(height: 1.0, color: ColorName.grey9Color),
+            16.height,
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                    text: TextSpan(children: [
+                      TextSpan(
+                        text: "Sch. Date: ",
+                        style: BaseText.baseTextStyle.copyWith(
+                          fontSize: 14,
+                          color: ColorName.dateTimeColor,
+                        ),
+                      ),
+                      TextSpan(
+                        text: "14/06/2024 - 15.30",
+                        style: BaseText.baseTextStyle.copyWith(
+                          fontSize: 14,
+                          fontWeight: BaseText.light,
+                          color: ColorName.dateTimeColor,
+                        ),
+                      )
+                    ]),
+                  ),
+                  8.height,
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(LocalImages.receiveIcon),
+                          const Dash(
+                            direction: Axis.vertical,
+                            dashLength: 3,
+                            length: 46,
+                          ),
+                          SvgPicture.asset(LocalImages.destinationIcon),
+                        ],
+                      ),
+                      16.width,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          buildPlaceStepper(
+                            label: "Receive From",
+                            location: "Main Storage Area",
+                          ),
+                          20.height,
+                          buildPlaceStepper(
+                            label: "Destination Location",
+                            location: "Medical Storage",
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: ColorName.backgroundColor,
+                child: Row(
+                  children: [
+                    Text(
+                      "Pallet ",
+                      style: BaseText.black2Text15
+                          .copyWith(fontWeight: BaseText.medium),
+                    ),
+                    Text(
+                      "(7)",
+                      style: BaseText.black2TextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: BaseText.regular,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -183,17 +204,21 @@ class _ReceiptDetailScreenState extends State<ReceiptDetailScreen> {
               width: 1,
             )),
         elevation: 0,
-        child: SizedBox(
+        child: Container(
+          height: 36,
           width: double.infinity,
-          child: Text(
-            label,
-            textAlign: TextAlign.center,
-            style: labelTextStyle ??
-                BaseText.baseTextStyle.copyWith(
-                  fontSize: 12,
-                  fontWeight: BaseText.medium,
-                  color: ColorName.grey12Color,
-                ),
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Center(
+            child: Text(
+              label,
+              textAlign: TextAlign.center,
+              style: labelTextStyle ??
+                  BaseText.baseTextStyle.copyWith(
+                    fontSize: 12,
+                    fontWeight: BaseText.medium,
+                    color: ColorName.grey12Color,
+                  ),
+            ),
           ),
         ));
   }
@@ -327,19 +352,32 @@ class _ReceiptDetailScreenState extends State<ReceiptDetailScreen> {
     );
   }
 
-  Column buildPlaceStepper({required String label, required String location}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
+  Widget buildPlaceStepper({required String label, required String location}) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            label,
+            style: BaseText.grey2Text13,
+          ),
+          Text(
+            location,
+            style: BaseText.grey10Text14.copyWith(fontWeight: BaseText.medium),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildPalletButtonSection() {
+    return Row(
       children: [
-        Text(
-          label,
-          style: BaseText.grey2Text13,
-        ),
-        Text(
-          location,
-          style: BaseText.grey10Text14.copyWith(fontWeight: BaseText.medium),
-        )
+        Flexible(child: buildOutlineButton(context, label: "Damage")),
+        12.width,
+        Flexible(child: buildOutlineButton(context, label: "Return")),
       ],
     );
   }

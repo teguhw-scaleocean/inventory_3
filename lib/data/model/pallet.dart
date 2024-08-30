10e1a6a8cf43b7e4d53c92c300420f6edc500759
teguh-scaleocean <teguh.scaleocean@gmail.com>
2024-08-30 11:38:56 +0700

    update-pallet-dropdown-height-handling-inprogress

--- lib/data/model/pallet.dart
+++ lib/data/model/pallet.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 // import 'package:inventory_v3/data/model/product.dart';
 
 // class Pallet {
@@ -6,4 +9,69 @@
 //   Product
 // }
 
+class Pallet {
+  int id;
+  String code;
+  Pallet({
+    required this.id,
+    required this.code,
+  });
 
+  Pallet copyWith({
+    int? id,
+    String? code,
+  }) {
+    return Pallet(
+      id: id ?? this.id,
+      code: code ?? this.code,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'code': code,
+    };
+  }
+
+  factory Pallet.fromMap(Map<String, dynamic> map) {
+    return Pallet(
+      id: map['id'] as int,
+      code: map['code'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Pallet.fromJson(String source) =>
+      Pallet.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'Pallet(id: $id, code: $code)';
+
+  @override
+  bool operator ==(covariant Pallet other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id && other.code == code;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ code.hashCode;
+}
+
+List<Pallet> pallets = [
+  Pallet(id: 1, code: "A490"),
+  Pallet(id: 2, code: "A491"),
+  Pallet(id: 3, code: "A492"),
+  Pallet(id: 4, code: "A493"),
+  Pallet(id: 5, code: "A494"),
+  Pallet(id: 6, code: "A495"),
+  Pallet(id: 7, code: "A496"),
+  Pallet(id: 8, code: "A497"),
+  Pallet(id: 9, code: "A498"),
+  Pallet(id: 10, code: "A499"),
+  Pallet(id: 11, code: "A4900"),
+  Pallet(id: 12, code: "A4901"),
+  Pallet(id: 13, code: "A4902"),
+];
--- lib/presentation/receipt/screens/receipt_detail_screen.dart
+++ lib/presentation/receipt/screens/receipt_detail_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -8,8 +10,12 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:inventory_v3/common/components/custom_app_bar.dart';
 import 'package:inventory_v3/common/components/custom_divider.dart';
 import 'package:inventory_v3/common/components/primary_button.dart';
+import 'package:inventory_v3/common/components/reusable_bottom_sheet.dart';
+import 'package:inventory_v3/common/components/reusable_dropdown_menu.dart';
+import 'package:inventory_v3/common/components/reusable_dropdown_search.dart';
 import 'package:inventory_v3/common/components/reusable_floating_action_button.dart';
 import 'package:inventory_v3/common/constants/local_images.dart';
+import 'package:inventory_v3/data/model/pallet.dart';
 import 'package:inventory_v3/data/model/product.dart';
 import 'package:inventory_v3/data/model/receipt.dart';
 import 'package:inventory_v3/presentation/receipt/screens/receipt_product_detail.dart';
@@ -31,12 +37,17 @@ class ReceiptDetailScreen extends StatefulWidget {
 }
 
 class _ReceiptDetailScreenState extends State<ReceiptDetailScreen> {
+  final TextEditingController _searchController = TextEditingController();
   late Receipt receipt;
   List<Product> listProducts = <Product>[];
 
   String date = "";
   String time = "";
   String _scanBarcode = "0.00";
+
+  List<dynamic> palletUpdates = [];
+
+  var selectedUpdatePallet;
 
   @override
   void initState() {
@@ -71,6 +82,15 @@ class _ReceiptDetailScreenState extends State<ReceiptDetailScreen> {
 
     date = receipt.dateTime.substring(0, 10);
     time = receipt.dateTime.substring(13, 18);
+
+    pallets.map((e) {
+      // if (e.id < 5) {
+      palletUpdates.add(e.code);
+      // }
+    }).toList();
+
+    // palletUpdates.sublist(0, 1);
+    log("palletUpdates: ${palletUpdates.length}");
   }
 
   // Future<void> scanBarcodeNormal() async {
@@ -197,27 +217,77 @@ class _ReceiptDetailScreenState extends State<ReceiptDetailScreen> {
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 16.w),
               child: buildScanAndUpdateSection(
-                onScan: () async {
-                  final scanResult = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const QRViewExample(),
-                    ),
-                  ).then((value) {
-                    if (value != null) {
-                      setState(() {
-                        _scanBarcode = value;
-                      });
-                      debugPrint("scanResultValue: $value");
+                  status: receipt.status,
+                  onScan: () async {
+                    final scanResult = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const QRViewExample(),
+                      ),
+                    ).then((value) {
+                      if (value != null) {
+                        setState(() {
+                          _scanBarcode = value;
+                        });
+                        debugPrint("scanResultValue: $value");
 
-                      Future.delayed(const Duration(seconds: 2), () {
-                        onShowSuccessDialog();
-                      });
-                    }
-                  });
-                },
-                status: receipt.status,
-              ),
+                        Future.delayed(const Duration(seconds: 2), () {
+                          onShowSuccessDialog();
+                        });
+                      }
+                    });
+                  },
+                  onUpdate: () {
+                    reusableBottomSheet(context, isShowDragHandle: false,
+                        StatefulBuilder(builder: (context, updateSetState) {
+                      var dropdownGap = 100.h;
+                      var submitButtonGap = 48.h;
+
+                      return Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.w),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Center(child: reusableDragHandle()),
+                              SizedBox(height: 16.h),
+                              reusableTitleBottomSheet(context,
+                                  title: "Update Pallet", isMainColor: false),
+                              SizedBox(height: 24.h),
+                              ReusableDropdownMenu(
+                                label: "Pallet",
+                                maxHeight: 320.h,
+                                offset: const Offset(0, -16),
+                                controller: _searchController,
+                                borderColor: ColorName.grey9Color,
+                                listOfItemsValue: palletUpdates,
+                                selectedValue: selectedUpdatePallet,
+                                onChange: (v) {},
+                                onTap: (onTapValue) {
+                                  updateSetState(() {
+                                    // selectedUpdatePallet = onTapValue;
+                                    // dropdownGap = 142.h;
+                                    // submitButtonGap = 48.h;
+                                  });
+                                },
+                              ),
+                              (palletUpdates.length > 4)
+                                  ? Container(color: Colors.blue, height: 600.h)
+                                  : Container(height: dropdownGap),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    top: submitButtonGap, bottom: 24.h),
+                                child: PrimaryButton(
+                                  onPressed: () {},
+                                  height: 40.h,
+                                  title: "Submit",
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    }));
+                  }),
             ),
             SizedBox(height: 16.h),
             const CustomDivider(height: 1.0, color: ColorName.grey9Color),
@@ -353,7 +423,8 @@ class _ReceiptDetailScreenState extends State<ReceiptDetailScreen> {
     );
   }
 
-  Row buildScanAndUpdateSection({required String status, Function()? onScan}) {
+  Row buildScanAndUpdateSection(
+      {required String status, Function()? onScan, Function()? onUpdate}) {
     Color? scanButtonColor;
     Color? updateButtonColor;
 
@@ -392,17 +463,20 @@ class _ReceiptDetailScreenState extends State<ReceiptDetailScreen> {
         ),
         SizedBox(width: 16.w),
         Flexible(
-          child: DisableButton(
-            height: 40.h,
-            width: double.infinity,
-            // width: 156,
-            iconWidget: SvgPicture.asset(
-              LocalImages.updatePalleteIcons,
-              height: 16.w,
-              width: 16.w,
+          child: GestureDetector(
+            onTap: onUpdate,
+            child: DisableButton(
+              height: 40.h,
+              width: double.infinity,
+              // width: 156,
+              iconWidget: SvgPicture.asset(
+                LocalImages.updatePalleteIcons,
+                height: 16.w,
+                width: 16.w,
+              ),
+              title: "Update Pallet",
+              color: updateButtonColor,
             ),
-            title: "Update Pallet",
-            color: updateButtonColor,
           ),
         ),
       ],

e2e8403798f4a0e6f5f93b0b994b69376f8dd6aa
teguh-scaleocean <teguh.scaleocean@gmail.com>
2024-09-10 11:53:24 +0700

    fix-top-logo-powered-by

--- lib/common/components/top_logo_section.dart
+++ lib/common/components/top_logo_section.dart
@@ -2,12 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../constants/local_images.dart';
+import '../theme/text/base_text.dart';
 
 Widget buildTopLogoSection() {
-  return SvgPicture.asset(
-    LocalImages.logoImage,
+  return Container(
     height: 80,
     width: 90,
-    fit: BoxFit.scaleDown,
+    padding: const EdgeInsets.symmetric(vertical: 24),
+    // decoration: BoxDecoration(
+    //   border: Border.all(
+    //     color: Colors.black,
+    //   ),
+    // ),
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          // crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              "Powered by",
+              textAlign: TextAlign.right,
+              style: BaseText.grey2Text10.copyWith(
+                fontWeight: BaseText.regular,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 4),
+        SvgPicture.asset(
+          LocalImages.logoImage,
+          // fit: BoxFit.scaleDown,
+        ),
+      ],
+    ),
   );
 }
